e3d64e1b8ab81a41c40b7ccfa53c0e360f1dd860
Galessss <marcus.balbino.2001@gmail.com>
2025-03-24 10:25:04 -0300

    dasdasdasdsafnjnafasnjsafasnkfnasinasfksanfsanfasn vai tomar no cu WSL aaaaaaaaaaaaaaaaaaa

--- blog/appBlog/views.py
+++ blog/appBlog/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponse
+from django.shortcuts import render , get_list_or_404
 from .models import Post
+
 
 def lista_posts(request):
     posts = Post.objects.order_by('-data_publicacao')[:10]
     return render(request, 'lista_posts.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_list_or_404(Post, pk=pk)
+    
+    return render(request, 'blog/lista_posts.html', {'post':post})
