42c9d2ea419be4179bad59a0c9fd254598a96d4c
WEB <jonatar75@gmail.com>
2025-03-24 10:55:24 -0300

    correcoes do settings

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'appBlog/Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
