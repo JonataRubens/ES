376fc1471f6b6a3826ff3b1dc116280af3536126
WEB <jonatar75@gmail.com>
2025-03-25 11:44:29 -0300

    pondo tudo no padrao e polindo

--- blog/appBlog/views.py
+++ blog/appBlog/views.py
@@ -3,11 +3,11 @@ from .models import Post
 from django.shortcuts import get_object_or_404
 
 
-def lista_posts(request):
+def ListaPosts(request):
     posts = Post.objects.order_by('-data_publicacao')[:10]
-    return render(request, 'lista_posts.html', {'posts': posts})
+    return render(request, 'ListaPosts.html', {'posts': posts})
 
 
-def test_link(request, id):
+def Link(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'Post.html',{'post':post})
